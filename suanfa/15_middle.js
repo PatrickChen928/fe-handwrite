6f33353aaa6b32dab0566b10a5064acf2b43d177
chen <786156072@qq.com>
2022-04-12 21:54:00 +0800

    15

---
+++ suanfa/15_middle.js
@@ -0,0 +1,40 @@
+// https://leetcode-cn.com/problems/3sum/
+// 15. 三数之和
+
+
+const input = [0,0,0,0];
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ var threeSum = function(nums) {
+  // 排序 + 双指针
+  let res = [];
+  const n = nums.length;
+  if (n < 3) {
+    return res;
+  }
+  nums = nums.sort((a, b) => a - b);
+  for (let i = 0; i < n; i++) {
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let L = i + 1;
+    let R = n - 1;
+    while (L < R) {
+      let sum = nums[i] + nums[L] + nums[R];
+      if (sum === 0) {
+        res.push([nums[i], nums[L], nums[R]]);
+      }
+      if (sum < 0) {
+        L++;
+        while(L < R && nums[L] === nums[L - 1]) L++;
+      } else {
+        R--;
+        while(L < R && nums[R + 1] === nums[R]) R--;
+      }
+    }
+  }
+  return res;
+};
+
+console.log(threeSum(input));
