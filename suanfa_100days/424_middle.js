93dc539a59794f00eba639923fe005eb14e80548
chen <786156072@qq.com>
2022-04-25 22:45:31 +0800

    17

--- suanfa_100days/424_middle.js
+++ suanfa_100days/424_middle.js
@@ -6,8 +6,22 @@
  * @param {number} k
  * @return {number}
  */
- var characterReplacement = function(s, k) {
-
+var characterReplacement = function(s, k) {
+  const nums = new Array(26).fill(0);
+  const ACode = 'A'.charCodeAt();
+  const n = s.length;
+  let left = 0, right = 0, max = 0;
+  while (right < n) {
+    let index = s[right].charCodeAt() - ACode;
+    nums[index]++;
+    max = Math.max(max, nums[index]);
+    if (right - left + 1 - max > k) {
+      nums[s[left].charCodeAt() - ACode]--;
+      left++;
+    }
+    right++;
+  }
+  return right - left;
 };
 
-console.log(characterReplacement("ABAB"))
+console.log(characterReplacement("BAAA", 0))
