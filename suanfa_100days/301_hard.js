fc5800c374f241b8141e3fd5564b5aa59e01d0c3
chen <786156072@qq.com>
2022-04-29 18:26:22 +0800

    20

--- suanfa_100days/301_hard.js
+++ suanfa_100days/301_hard.js
@@ -9,45 +9,45 @@ var removeInvalidParentheses = function(s) {
   const n = s.length;
   const res = [];
   // 1. 回溯 + 剪枝
-  let lRemove = 0, rRemove = 0;
-  for (let i = 0; i < n; i++) {
-    if (s[i] === '(') {
-      lRemove++;
-    } else if (s[i] === ')') {
-      if (lRemove === 0) {
-        rRemove++;
-      } else {
-        lRemove--;
-      }
-    }
-  }
-  if (lRemove === 0 && rRemove === 0 && isValid(s)) {
-    return [s];
-  }
-  function helper(str, start, lRemove, rRemove) {
-    // 剪枝
-    if (lRemove === 0 && rRemove === 0) {
-      if (isValid(str)) {
-        res.push(str);
-        return;
-      }
-    }
-    for (let i = start; i < str.length; i++) {
-      if (i > start && str[i] === str[i - 1]) {
-        continue;
-      }
-      // 剪枝
-      if (lRemove + rRemove > str.length - i) {
-        return;
-      }
-      if (lRemove > 0 && str[i] === '(') {
-        helper(str.substr(0, i) + str.substr(i + 1), i, lRemove - 1, rRemove);
-      }
-      if (rRemove > 0 && str[i] === ')') {
-        helper(str.substr(0, i) + str.substr(i + 1), i, lRemove, rRemove - 1);
-      }
-    }
-  }
+  // let lRemove = 0, rRemove = 0;
+  // for (let i = 0; i < n; i++) {
+  //   if (s[i] === '(') {
+  //     lRemove++;
+  //   } else if (s[i] === ')') {
+  //     if (lRemove === 0) {
+  //       rRemove++;
+  //     } else {
+  //       lRemove--;
+  //     }
+  //   }
+  // }
+  // if (lRemove === 0 && rRemove === 0 && isValid(s)) {
+  //   return [s];
+  // }
+  // function helper(str, start, lRemove, rRemove) {
+  //   // 剪枝
+  //   if (lRemove === 0 && rRemove === 0) {
+  //     if (isValid(str)) {
+  //       res.push(str);
+  //       return;
+  //     }
+  //   }
+  //   for (let i = start; i < str.length; i++) {
+  //     if (i > start && str[i] === str[i - 1]) {
+  //       continue;
+  //     }
+  //     // 剪枝
+  //     if (lRemove + rRemove > str.length - i) {
+  //       return;
+  //     }
+  //     if (lRemove > 0 && str[i] === '(') {
+  //       helper(str.substr(0, i) + str.substr(i + 1), i, lRemove - 1, rRemove);
+  //     }
+  //     if (rRemove > 0 && str[i] === ')') {
+  //       helper(str.substr(0, i) + str.substr(i + 1), i, lRemove, rRemove - 1);
+  //     }
+  //   }
+  // }
 
   function isValid(str) {
     let ln = 0;
@@ -65,12 +65,32 @@ var removeInvalidParentheses = function(s) {
     return ln === 0;
   }
 
-  helper(s, 0, lRemove, rRemove);
-  return res;
+  // helper(s, 0, lRemove, rRemove);
+  // return res;
 
   // 2. 广度优先搜索
+  const ans = [];
+  let currSet = new Set();
+  currSet.add(s);
+  while(currSet.size) {
+    for (let str of currSet) {
+      if (isValid(str)) {
+        ans.push(str);
+      }
+    }
+    if (ans.length) {
+      return ans;
+    }
+    let nextSet = new Set();
+    for (let str of currSet) {
+      for (let i = 0; i < str.length; i++) {
+        if (str[i] === ')' || str[i] === '(') {
+          nextSet.add(str.substr(0, i) + str.substr(i + 1));
+        }
+      }
+    }
+    currSet = nextSet;
+  }
 };
 
 console.log(removeInvalidParentheses("(((k()(("))
-
-'()()()', '(())()'
