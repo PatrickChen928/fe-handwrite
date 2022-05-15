4d656885a3a5d777014df75570c4251cf669b298
chen <786156072@qq.com>
2022-05-15 13:10:33 +0800

    test

---
+++ suanfa_100days/5_middle.js
@@ -0,0 +1,12 @@
+// https://leetcode.cn/problems/longest-palindromic-substring/
+// 5. 最长回文子串
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+
+};
+
+console.log(longestPalindrome("babad"));
