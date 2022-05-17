5508d694d29c1e0f00ee9467be2559fc1a6171a2
chen <786156072@qq.com>
2022-05-18 00:01:29 +0800

    chore: 10_hard

--- suanfa_100days/10_hard.js
+++ suanfa_100days/10_hard.js
@@ -7,25 +7,47 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
-  const m = s.length;
-  const n = p.length;
-  const dp = new Array(m + 1).fill('').map(() => new Array(n + 1).fill(false));
-  dp[0][0] = true;
-  for (let i = 0; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (p[j - 1] === '*') {
-        dp[i][j] = dp[i][j - 2];
-        if (matchs(s, p, i, j - 1)) {
-          dp[i][j] = dp[i][j] || dp[i - 1][j];
-        }
-      } else {
-        if (matchs(s, p, i, j)) {
-          dp[i][j] = dp[i - 1][j - 1];
-        }
-      }
+  // const m = s.length;
+  // const n = p.length;
+  // const dp = new Array(m + 1).fill('').map(() => new Array(n + 1).fill(false));
+  // dp[0][0] = true;
+  // for (let i = 0; i <= m; i++) {
+  //   for (let j = 1; j <= n; j++) {
+  //     if (p[j - 1] === '*') {
+  //       dp[i][j] = dp[i][j - 2];
+  //       if (matchs(s, p, i, j - 1)) {
+  //         dp[i][j] = dp[i][j] || dp[i - 1][j];
+  //       }
+  //     } else {
+  //       if (matchs(s, p, i, j)) {
+  //         dp[i][j] = dp[i - 1][j - 1];
+  //       }
+  //     }
+  //   }
+  // }
+  // return dp[m][n];
+
+  const n = s.length, m = p.length;
+  const f = Array.from({ length: n + 1 }, () => new Array(m + 1).fill(false));
+  f[0][0] = true;
+  
+  s = ' ' + s;
+  p = ' ' + p;
+  
+  for (let i = 0; i <= n; i++) {
+    for (let j = 1; j <= m; j++) {
+      if (j + 1 <= m && p[j + 1] === '*') continue;
+      if (i !== 0 && p[j] !== '*')
+        // 正常情况 
+        f[i][j] = f[i - 1][j - 1] && (s[i] === p[j] || p[j] === '.');
+      if (p[j] === '*')
+        // 1. ab abb*
+        // 2. ab ab* / ac ab*
+        f[i][j] = f[i][j - 2] || (i !== 0 && f[i - 1][j] && (s[i] === p[j - 1] || p[j - 1] === '.'));
     }
   }
-  return dp[m][n];
+  console.log(f)
+  return f[n][m];
 };
 
 function matchs(s, p, i, j) {
@@ -38,4 +60,4 @@ function matchs(s, p, i, j) {
   return s.charAt(i - 1) == p.charAt(j - 1);
 }
 
-console.log(isMatch("aa", ".*ab"));
+console.log(isMatch("aa", "a*ab"));
