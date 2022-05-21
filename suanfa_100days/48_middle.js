580cddd9b24e31b4a5cfcc6600c907e0f93d0407
chen <786156072@qq.com>
2022-05-21 10:00:22 +0800

    71

---
+++ suanfa_100days/48_middle.js
@@ -0,0 +1,21 @@
+// https://leetcode.cn/problems/rotate-image/
+// 48. 旋转图像
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+  const n = matrix.length;
+  for (let i = 0; i < n >> 1; i++) {
+    for (let j = 0; j < Math.ceil(n / 2); j++) {
+      const temp = matrix[i][j];
+      matrix[i][j] =  matrix[n - j - 1][i];
+      matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+      matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+      matrix[j][n - i - 1] = temp;
+    }
+  }
+};
+
+console.log(rotate([[1,2,3],[4,5,6],[7,8,9]]));
