344ca7245e231b029f53d1239438194eb533567e
chen <786156072@qq.com>
2022-05-23 20:49:13 +0800

    74

---
+++ suanfa_100days/62_middle.js
@@ -0,0 +1,19 @@
+// https://leetcode.cn/problems/unique-paths/
+// 62. 不同路径
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+  const dp = new Array(n).fill(1);
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[j] = dp[j] + dp[j - 1];
+    }
+  }
+  return dp[n - 1];
+};
+
+console.log(uniquePaths(1, 1));
