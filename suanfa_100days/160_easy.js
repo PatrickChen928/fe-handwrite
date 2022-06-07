4f0f5b688e894cbe376ddfa3c9f717e2daeeb20c
chen <786156072@qq.com>
2022-06-07 23:59:10 +0800

    91

---
+++ suanfa_100days/160_easy.js
@@ -0,0 +1,19 @@
+// https://leetcode.cn/problems/intersection-of-two-linked-lists/
+// 160. 相交链表
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    
+};
