5d3005117a64747d8e8036a403b7d4dc8f200cfd
chen <786156072@qq.com>
2022-06-08 20:54:03 +0800

    add

--- suanfa_100days/160_easy.js
+++ suanfa_100days/160_easy.js
@@ -15,5 +15,13 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    
+  if (headA === null || headB === null) {
+      return null;
+  }
+  let pA = headA, pB = headB;
+  while (pA !== pB) {
+      pA = pA === null ? headB : pA.next;
+      pB = pB === null ? headA : pB.next;
+  }
+  return pA;
 };
