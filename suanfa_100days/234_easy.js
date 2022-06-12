00c359756cfbb5711cc593a799b4e7fb81a9121c
chen <786156072@qq.com>
2022-06-12 14:43:50 +0800

    96

--- suanfa_100days/234_easy.js
+++ suanfa_100days/234_easy.js
@@ -13,30 +13,24 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-  const reverseList = node => {
-    let prev = null, curr = node;
-    while(curr != null) {
-      const temp = curr.next;
-      curr.next = prev;
-      prev = curr;
-      curr = temp;
+  if (head === null || head.next === null) return true;
+    let prev = null, slow = fast = head;
+    while(fast && fast.next) {
+        fast = fast.next.next;
+        const temp = slow;
+        slow = slow.next;
+        temp.next = prev;
+        prev = temp;
     }
-    return prev;
-  }
-  const getMiddleNode = node => {
-    let slow = fast = node;
-    while(fast != null && fast.next != null) {
-      slow = slow.next;
-      fast = fast.next.next;
+    if (fast) {
+        slow = slow.next;
     }
-    return slow;
-  }
-  const mid = getMiddleNode(head);
-  let reverseMid = reverseList(mid);
-  while(reverseMid != null) {
-    if (reverseMid.val != head.val) return false;
-    reverseMid = reverseMid.next;
-    head = head.next;
-  }
-  return true;
+    while (slow) {
+        if (slow.val != prev.val) {
+        return false;
+        }
+        slow = slow.next;
+        prev = prev.next;
+    }
+    return true;
 };
