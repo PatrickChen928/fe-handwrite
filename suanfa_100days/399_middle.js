6177a1d80a74ddaf0e0130e95852d863d03dec32
cyly <786156072@qq.com>
2022-07-06 22:47:42 +0800

    114

--- suanfa_100days/399_middle.js
+++ suanfa_100days/399_middle.js
@@ -7,63 +7,56 @@
  * @param {string[][]} queries
  * @return {number[]}
  */
- var calcEquation = function(equations, values, queries) {
-  let nvars = 0;
-  const variables = new Map();
-
+var calcEquation = function(equations, values, queries) {
+  let id = 0;
   const n = equations.length;
+  const keyMap = new Map();
+  
   for (let i = 0; i < n; i++) {
-    if (!variables.has(equations[i][0])) {
-      variables.set(equations[i][0], nvars++);
+    const [key1, key2] = equations[i];
+    if (!keyMap.has(key1)) {
+      keyMap.set(key1, id++);
     }
-    if (!variables.has(equations[i][1])) {
-        variables.set(equations[i][1], nvars++);
+    if (!keyMap.has(key2)) {
+      keyMap.set(key2, id++);
     }
   }
-  // parent
-  const f = new Array(nvars).fill(0).map((val, index) => index);
-  // weight
-  const w = new Array(nvars).fill(1.0);
-
-  for (let i = 0; i < n; i++) {
-      const va = variables.get(equations[i][0]), vb = variables.get(equations[i][1]);
-      merge(f, w, va, vb, values[i]);
+  const parent = new Array(id).fill(0).map((_, index) => index);
+  const weight = new Array(id).fill(1);
+  const find = function(id) {
+    if(parent[id] != id) {
+      const p = find(parent[id]);
+      weight[id] *= weight[parent[id]];
+      parent[id] = p;
+    }
+    return parent[id];
   }
-  console.log(f, w)
-  const queriesCount = queries.length;
-  const ret = new Array(queriesCount).fill(0);
-  for (let i = 0; i < queriesCount; i++) {
-    const query = queries[i];
-    let result = -1.0;
-    if (variables.has(query[0]) && variables.has(query[1])) {
-      const ia = variables.get(query[0]), ib = variables.get(query[1]);
-      const fa = findf(f, w, ia), fb = findf(f, w, ib);
-      if (fa == fb) {
-        result = w[ia] / w[ib];
+  const union = function(idx, idy, val) {
+    const rootX = find(idx);
+    const rootY = find(idy);
+    parent[rootX] = rootY;
+    weight[rootX] = weight[idy] * val / weight[idx];
+  }
+  for (let i = 0; i < n; i++) {
+    const aId = keyMap.get(equations[i][0]), bId = keyMap.get(equations[i][1]);
+    union(aId, bId, values[i])
+  }
+  console.log(parent, weight)
+  const ans = [];
+  for (let i = 0; i < queries.length; i++) {
+    const q = queries[i];
+    let result = -1;
+    if (keyMap.has(q[0]) && keyMap.has(q[1])) {
+      const a = keyMap.get(q[0]), b = keyMap.get(q[1]);
+      const idx = find(a), idy = find(b);
+      if (idx === idy) {
+        result = weight[a] / weight[b];
       }
     }
-    ret[i] = result;
+    ans[i] = result;
   }
-  return ret;
+  return ans;
 }
 
-const merge = (f, w, x, y, val) => {
-  const fx = findf(f, w, x);
-  const fy = findf(f, w, y);
-  f[fx] = fy;
-  w[fx] = val * w[y] / w[x];
-}
-
-const findf = (f, w, x) => {
-  if (f[x] != x) {
-    const father = findf(f, w, f[x]);
-    w[x] = w[x] * w[f[x]];
-    f[x] = father;
-  }
-  return f[x];
-};
-
-// a -> b -> c 
-// a -> c 
-// b -> c
 console.log(calcEquation([["a","b"],["b","c"]], [2.0,3.0], [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]))
+// console.log(calcEquation([["a","b"],["a","d"],["d","c"]], [3.0,6.0,4.0], [["b","c"]]))
